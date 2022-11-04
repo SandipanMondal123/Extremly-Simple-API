dbae1fe0d14060cc7b98b7ca23829035601702c6
Sandipan Mondal <office.smondal@gmail.com>
2022-11-04 15:27:17 -0400

    get endpoint

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const PORT = 8080
 
+console.log('hello')
 app.use(express.json()); // converts body to JSON
 
+app.get("", (req, res) => {
+    res.status(200).send({"message": "This is the get request" })
+})
 
+app.post("/book/:id", (req, res) => {
+    const { id } = req.params;
+    const {body} = req.body;
+    res.status(200).send({message: `this is your post response ${id}`, body: `this is your body message -> ${body}`})
+
+})
+
+app.listen(PORT || 50000, () => console.log(`RUNNING on PORT ${PORT}`))
